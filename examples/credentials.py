7ed4594f6b8e1bbb2ce810b4f46140e84dfeb952
Elias Ankerhold <elias.ankerhold@gmail.com>
2024-11-19 18:54:26 +0200

    added minimal example

---
+++ examples/credentials.py
@@ -0,0 +1,4 @@
+API_KEY = '123456789abcdefghijklmnopqrstuvwxyz'
+IP_ADDRESS = '123.456.789.0'
+PORT_NUMBER = 12345
+MXC_ID = 6
---
+++ examples/read_example.py
@@ -0,0 +1,20 @@
+from blueftc.BlueforsController import BlueFTController
+from credentials import IP_ADDRESS, PORT_NUMBER, API_KEY, MXC_ID
+
+import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+controller = BlueFTController(ip=IP_ADDRESS, port=PORT_NUMBER, key=API_KEY, mixing_chamber_channel_id=MXC_ID)
+
+active_channels = [1, 2, 5, 6, 8]
+
+for ch in active_channels:
+	print(f"Channel {ch} temp: {controller.get_channel_temperature(ch)} Kelvin")
+	print(f"Channel {ch} resistance: {controller.get_channel_resistance(ch)} Ohm")
+
+print(f"MXC heater status: {controller.get_mxc_heater_status()}")
+print(f"MXC heater power: {controller.get_mxc_heater_power()} uW")
+print(f"MXC heater PID: {controller.get_mxc_heater_mode()}")
+print(f"MXC heater setpoint: {controller.get_mxc_heater_setpoint()} K")
